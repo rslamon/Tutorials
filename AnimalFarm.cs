1091f1e7c3d4dc7e196fae2376bd1282266adc98
rslamon <44618469+rslamon@users.noreply.github.com>
2019-01-10 15:38:26 -0500

    Add files via upload

---
+++ AnimalFarm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial14
+{
+    class AnimalFarm : IEnumerable
+    {
+        private List<Animal> animalList = new List<Animal>();
+
+        public AnimalFarm(List<Animal> animalList)
+        {
+            this.animalList = animalList;
+        }
+
+        public AnimalFarm()
+        {
+
+        }
+           public Animal this[int index]
+        {
+            get { return (Animal)animalList[index]; }
+            set { animalList.Insert(index, value); }
+        }
+
+        public int Count
+        {
+            get { return animalList.Count; }
+        }
+
+      IEnumerator IEnumerable.GetEnumerator()
+        {
+            return animalList.GetEnumerator();
+        }
+        
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AnimalFarm myAnimals = new AnimalFarm();
+             myAnimals[0] = new Animal("Wilbur");
+            myAnimals[1] = new Animal("Templeton");
+            myAnimals[2] = new Animal("Gander");
+            myAnimals[3] = new Animal("Charlotte");
+
+            foreach( Animal i in myAnimals)
+            {
+                Console.WriteLine(i.Name);
+            }
+             Box box1 = new Box(2, 3, 4);
+            Box box2 = new Box(5, 6, 7);
+
+            Box box3 = box1 + box2;
+
+            Console.WriteLine($"Box 3 : {box3}");
+
+            Console.WriteLine($"Box int : {(int)box3}");
+
+            Box box4 = (Box)4;
+
+            Console.WriteLine($"Box 4 : {(Box)4}");
+
+            var shopkins = new { Name = "Shopkins", Price = 4.99 };
+
+            Console.WriteLine("{0} cost ${1}", shopkins.Name, shopkins.Price);
+
+            var toyArray = new[] {new
+            {
+                Name = "Yo Kai Pack", Price = 4.99 }, new  { Name = "Legos", Price = 9.99}
+            };
+
+            foreach( var item in toyArray)
+            {
+                Console.WriteLine("{0} costs ${1}" , item.Name, item.Price);
+            }
+        
+            Console.ReadLine();
+
+        }
+    }
+}

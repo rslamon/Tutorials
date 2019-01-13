5cce4c063545865ef385f81c4115ae9cc286bffc
rslamon <44618469+rslamon@users.noreply.github.com>
2019-01-12 20:43:55 -0500

    Add files via upload

---
+++ Tutorial 15/Tutorial 15/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_15
+{
+    class Owner
+    {
+        public string Name { get; set; }
+        public int OwnerID { get; set; }
+
+    }
+}

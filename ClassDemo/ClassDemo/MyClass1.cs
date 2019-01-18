ff5a7641b9c71045bf24f252a0a909d1a7deba14
rslamon <44618469+rslamon@users.noreply.github.com>
2019-01-18 11:16:00 -0500

    Add files via upload

---
+++ ClassDemo/ClassDemo/MyClass1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class MyClass1
+    {
+
+        //Non staic memebers
+        public string message = "Hello World";
+        public string Name { get; set; }
+
+        public void DisplayName()
+        {
+            Console.WriteLine("Name = {0}", Name);
+        }
+
+        //static memebers
+
+        public static string greetings = "Good Morning";
+        public static int Age { get; set; }
+        public static void DisplayAge()
+        {
+            Console.WriteLine("Age={0}",Age);
+        }
+    }
+}
---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int pay;
+
+            //Staff staff1 = new Staff("Peter");
+            //staff1.HoursWorked = 160;
+            //pay = staff1.CalculatePay(1000, 400);
+            //Console.WriteLine("Pay = {0}",pay);
+
+            //Staff staff2 = new Staff("Jane");
+            //staff2.HoursWorked = 160;
+            //pay = staff2.CalculatePay();
+            //Console.WriteLine("Pay = {0}", pay);
+
+            //Staff staff3 = new Staff("Carol");
+            //staff3.HoursWorked = -10;
+            //pay = staff3.CalculatePay();
+            //Console.WriteLine("Pay = {0}", pay);
+
+            MyClass1 classA = new MyClass1();
+            Console.WriteLine(classA.message);
+            classA.Name = "Jamie";
+            classA.DisplayName();
+
+            Console.WriteLine(MyClass1.greetings);
+            MyClass1.Age = 39;
+            MyClass1.DisplayAge();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

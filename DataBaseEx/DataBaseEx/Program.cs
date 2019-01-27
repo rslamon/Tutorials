9f7dd638719470535d4699c000b4bc5286987557
rslamon <44618469+rslamon@users.noreply.github.com>
2019-01-26 20:57:35 -0500

    Add files via upload

---
+++ DataBaseEx/DataBaseEx/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Configuration;
+
+
+namespace DataBaseEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+            using (DbConnection connection =
+                factory.CreateConnection())
+            {
+                if(connection == null)
+                {
+                    Console.WriteLine("Connection Error");
+                    Console.ReadLine();
+                    return;
+                }
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+
+                if(command == null)
+                {
+                    Console.WriteLine("Command Error");
+                    Console.ReadLine();
+                    return;
+                }
+
+                command.Connection = connection;
+
+                command.CommandText = "Select * from Products";
+
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($"{dataReader["prodID"]} " + $"{dataReader["Product"] }");
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}

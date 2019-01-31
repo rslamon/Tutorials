07d67dff59a58ce97bbebf44db4a0256d7d9bbdd
rslamon <44618469+rslamon@users.noreply.github.com>
2019-01-31 16:45:20 -0500

    Add files via upload

---
+++ Classroom 4/Classroom 4/Classroom5/Classroom5/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Classroom5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.txtRemove = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.listBooze = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(343, 61);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd.TabIndex = 1;
+            // 
+            // txtRemove
+            // 
+            this.txtRemove.Location = new System.Drawing.Point(343, 90);
+            this.txtRemove.Name = "txtRemove";
+            this.txtRemove.Size = new System.Drawing.Size(100, 20);
+            this.txtRemove.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(467, 57);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "&Load Item";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(467, 88);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // listBooze
+            // 
+            this.listBooze.FormattingEnabled = true;
+            this.listBooze.Location = new System.Drawing.Point(343, 160);
+            this.listBooze.Name = "listBooze";
+            this.listBooze.Size = new System.Drawing.Size(120, 95);
+            this.listBooze.TabIndex = 5;
+            this.listBooze.SelectedIndexChanged += new System.EventHandler(this.listBooze_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBooze);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtRemove);
+            this.Controls.Add(this.txtAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.TextBox txtRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox listBooze;
+    }
+}
+
---
+++ Classroom 4/Classroom 4/Classroom5/Classroom5/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classroom5
+{
+    public partial class Form1 : Form
+    {
+        public string[] sNames = new string[] { "Bob", "Mike","Steve","Larry","Sally" };
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            FillList();
+        }
+
+      
+        private void FillList()
+        {
+           
+
+            for (int i =0; i < sNames.Length; i++)
+            {
+                listBooze.Items.Add(sNames[i]);
+               
+            }
+        }
+
+        private void RemoveList()
+        {
+           for(int a = 0; a <= listBooze.Items.Count; a++)
+            {
+                if(txtRemove.Text == Convert.ToString(listBooze.Items[a]))
+                {
+                    listBooze.Items.Remove(txtRemove.Text);
+                }
+            }
+        }
+           
+
+                private void btnRemove_Click(object sender, EventArgs e)
+                {
+                   if (txtRemove.Text.Length > 0)
+            {
+                RemoveList();
+            }
+            else
+            {
+                MessageBox.Show("You need to enter a name");
+                return;
+            }
+
+                }
+
+        private void listBooze_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have chosen " + Convert.ToString(listBooze.SelectedItem));
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(txtAdd.Text.Length > 0)
+            {
+                listBooze.Items.Add(txtAdd.Text);
+            }
+            else
+            {
+                MessageBox.Show("You need to add a name.");
+                return;
+            }
+            
+        }
+    }
+}
